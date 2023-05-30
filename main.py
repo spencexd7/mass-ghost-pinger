d4039afb490b000d68b07761cb117a5573429263
spenceya <131441794+spencexd7@users.noreply.github.com>
2023-05-30 09:09:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,13 @@
 # discord : https://discord.gg/gpqMWg5PAY
 
 
-
-
 import discord
 from discord.ext import commands
 import random
 import os
+import asyncio
+import aiohttp
+
 os.system("clear")
 print("""
         
@@ -37,25 +38,38 @@ client = commands.Bot(command_prefix='spencexd7',intents=discord.Intents.all())
 
 client._skip_check = lambda x, y: False
 
-async def send(ids, channel):
+async def send(ids, session, channel):
+  
     chunks = [ids[i:i+max] for i in range(0, len(ids), max)]
     for chunk in chunks:
         mems = [f"<@{id}>" for id in chunk]
         format = " ||-|| ".join(mems)
         msg = f"||{format}||"
-        he =await channel.send(msg)
-        await he.delete()
+        async with session.post(f"https://discord.com/api/v9/channels/{channel.id}/messages", json={"content": msg}) as response:
+            if response.status == 200:
+                message = await response.json()
+                dll = await channel.fetch_message(int(message['id']))
+                await dll.delete()
+
     if len(ids) % max != 0:
         left = ids[-(len(ids) % max):]
         membs = [f"<@{id}>" for id in left]
         formats = " ||-|| ".join(membs)
         message = f"||{formats}||"
-        hek= await channel.send(message)
-        await hek.delete()
-        tm = random.choice(['who tf pinged', 'burh pinged', 'pong', 'wtf', 'stop bro'])
-        await channel.send(tm)
+        async with session.post(f"https://discord.com/api/v9/channels/{channel.id}/messages", json={"content": message}) as response:
+            if response.status == 200:
+                message = await response.json()
+                dl = await channel.fetch_message(int(message['id']))
+                await dl.delete()
+                tm = random.choice(['who tf pinged', 'burh pinged', 'pong', 'wtf', 'stop bro'])
+                await channel.send(tm)
+
+
+headers = {'Authorization': f'{tk}'}
+
 @client.event
 async def on_ready():
+  async with aiohttp.ClientSession(headers=headers, connector=None) as ss:
     guild = client.get_guild(gl)
     members = guild.members
     with open("members.txt", "w") as f:
@@ -64,12 +78,11 @@ async def on_ready():
             print(member.id)
     with open("members.txt", "r") as f:
         mids = f.read().splitlines()
-      
-    cha = client.get_channel(int(ch))
-    print("starting...........")
-    while True:
-        await send(mids, cha)
+
+        channel = client.get_channel(int(ch))
+        print("starting...........")
+        while True:
+            await send(mids, ss, channel)
     
   
-    
-client.run(tk, bot=False)
+client.run(tk)
